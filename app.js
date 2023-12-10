043649fff5e3801b7e43dd78369a98188a6a8255
abusaeid69 <saeidabu456@gmail.com>
2023-12-10 21:37:40 +0600

    abcd

---
+++ app.js
@@ -0,0 +1,18 @@
+/*const number =[1, 2, 3, 4, 5, 6];
+for(i=0; i<number.length; i++){
+    console.log(number[i])
+} 
+
+
+let i =1;
+while(i <15){
+console.log(i);
+i++
+} */
+
+let i = 11;
+
+do{
+    console.log(i)
+    i++
+} while(i < 11)
